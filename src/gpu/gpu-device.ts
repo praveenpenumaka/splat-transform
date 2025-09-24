899ee7c81830dfc93e7e55fbf8df28e14782adc2
Donovan Hutchence <slimbuck7@gmail.com>
2025-09-24 09:19:48 +0100

    Update webgpu

--- src/gpu/gpu-device.ts
+++ src/gpu/gpu-device.ts
@@ -119,7 +119,7 @@ const createDevice = async () => {
 
     const graphicsDevice = new WebgpuGraphicsDevice(canvas, {
         antialias: false,
-        depth: true,
+        depth: false,
         stencil: false
     });
 
